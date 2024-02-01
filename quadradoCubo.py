5088e63642f6ffd0fe041495a341b3f595b33bab
Danilo Mariano <ssadanilo@hotmail.com>
2024-02-01 19:54:19 -0300

    quadradoCuboRaiz

---
+++ quadradoCubo.py
@@ -0,0 +1,8 @@
+numero = int(input("Digite um número: "))
+quadrado = numero ** 2
+cubo = numero ** 3
+raiz_quadrada = numero ** (1/2)
+
+print(f"{numero} ao quadrado é {quadrado}")
+print(f"{numero} ao cubo é {cubo}")
+print(f"A raiz quadrada de {numero} é {raiz_quadrada}")
